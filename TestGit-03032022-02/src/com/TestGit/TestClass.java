7af901ab1de9a07649b24d85a98be1df63447537
Apurav0104 <hingeapurav14@gmail.com>
2022-03-03 18:17:27 +0530

    first commit

---
+++ TestGit-03032022-02/src/com/TestGit/TestClass.java
@@ -0,0 +1,10 @@
+package com.TestGit;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("Hello World-22");
+		System.out.println("Hello cruel World");
+		System.out.println("Hello Futurist");
+	}
+
+}
